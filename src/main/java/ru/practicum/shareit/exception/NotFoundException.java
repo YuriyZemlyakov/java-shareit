acf9038eddb86281bfd2a4ffcb19bf70046f33f8
Yuriy Zemlyakov <yuriy.zemlyakov@yandex.ru>
2024-09-16 00:29:42 +0300

    fix unused import

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.service.annotation.HttpExchange;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
