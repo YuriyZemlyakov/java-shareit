0a32966970c7dae4a26c009d9d7386c47d95fbec
Yuriy Zemlyakov <yuriy.zemlyakov@yandex.ru>
2024-09-16 00:24:39 +0300

    fix code style

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
